ce7d4dd97e668f9d375df89b445485d8be88843b
Himanshu  Kansal <himanshu@trihund.com>
2019-09-07 12:37:46 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import './App.css';
 
+import { Route, Switch } from 'react-router-dom';
 
+// pages
 import Home from './pages/Home'
 import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
 
+// components
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <Home />
-      <Rooms />
-      <SingleRoom />
-      <Error />
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/rooms' component={Rooms} />
+        <Route exact path='/rooms/:slug' component={SingleRoom} />
+        <Route path='/' component={Error} />
+      </Switch>
     </>
   );
 }
